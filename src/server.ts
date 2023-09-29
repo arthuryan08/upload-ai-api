6666d7ac16ab8d24eab9d751d376c67ab8bce404
arthuryan08 <arthuryan08@gmail.com>
2023-09-29 18:46:16 -0300

    feat:create prisma models, migration and conection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { fastify } from 'fastify';
+import { prisma } from './lib/prisma';
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello world!'
+app.get('/prompts', async () => {
+  const prompts = await prisma.prompt.findMany()
+
+  return prompts
 })
 
 app.listen({ 
