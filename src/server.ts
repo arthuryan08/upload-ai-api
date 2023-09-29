4d9ef1d9d4e4a77136ed125cb1751d0861d08bd1
arthuryan08 <arthuryan08@gmail.com>
2023-09-29 20:27:54 -0300

    feat: upload route is done with fastify multipart

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from 'fastify';
 import { prisma } from './lib/prisma';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
 
 app.listen({ 
   port: 3333,
